7c0d6d1501ccc9cfa8e10f93f1cb93a3b3dafebf
Kamron <kboturkhonov@croc.ru>
2024-12-26 16:03:36 +0300

    fix

--- src/main/java/ru/itmo/concurrency/mapper/SortMapper.java
+++ src/main/java/ru/itmo/concurrency/mapper/SortMapper.java
@@ -7,12 +7,12 @@ import ru.itmo.concurrency.dto.SortDto;
 
 import java.io.IOException;
 
-public class SortMapper extends Mapper<Object, Text, LongWritable, SortDto> {
-    private final LongWritable outKey = new LongWritable();
+public class SortMapper extends Mapper<LongWritable, Text, LongWritable, SortDto> {
 
     @Override
-    protected void map(final Object key, final Text value, final Context context)
-            throws IOException, InterruptedException {
+    protected void map(final LongWritable key, final Text value, final Context context) throws
+            IOException,
+            InterruptedException {
         final String[] fields = value.toString().split("\t");
 
         if (fields.length == 3) {
@@ -20,8 +20,7 @@ public class SortMapper extends Mapper<Object, Text, LongWritable, SortDto> {
                 final String categoryKey = fields[0];
                 final long revenue = Long.parseLong(fields[1]);
                 final int quantity = Integer.parseInt(fields[2]);
-                outKey.set(revenue);
-                context.write(outKey, new SortDto(categoryKey, quantity));
+                context.write(new LongWritable(revenue), new SortDto(categoryKey, quantity));
             } catch (final NumberFormatException e) {
                 System.err.println("Skipping invalid record: " + value);
             }
--- src/main/java/ru/itmo/concurrency/util/SalesComparator.java
+++ src/main/java/ru/itmo/concurrency/util/SalesComparator.java
@@ -1,13 +1,17 @@
 package ru.itmo.concurrency.util;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class SalesComparator extends WritableComparator {
 
+    public SalesComparator() {
+        super(LongWritable.class, true);
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return b.compareTo(a);
     }
-
 }
